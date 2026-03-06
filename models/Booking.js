7533c538bc07d7768a81b03d27c82f28a1289bab
Mohamed Nabil <monabill211@gmail.com>
2026-03-06 07:37:47 +0200

    UP

--- models/Booking.js
+++ models/Booking.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
-    date: {
-      type: String,
-      required: true,
-    },
-    message: {
-      type: String,
-    },
+{
+  name: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
+  phone: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  note: {
+    type: String,
+  },
+},
+{ timestamps: true }
 );
 
 export default mongoose.model("Booking", bookingSchema);
